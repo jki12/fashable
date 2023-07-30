d67d17ae970a7f64d7ef30e816f6fa03ab0db5d7
Hwangseoeun <sunny030921@gmail.com>
2023-07-30 10:28:17 +0900

    복지 페이지 크롤링

---
+++ src/main/java/Hackerton/fashable/seoul/WelCrawling.java
@@ -0,0 +1,32 @@
+package Hackerton.fashable.seoul;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class WelCrawling {
+    public static void main(String[] args) {
+        String url = "https://news.seoul.go.kr/welfare/news-all";
+        Document doc = null;
+        System.out.println("URL= " + url);
+
+        try {
+            // 해당 URL의 HTML 문서를 가져옵니다.
+            Document document = Jsoup.connect(url).timeout(10000).get();
+
+            // 게시글 제목이 포함된 HTML 요소를 선택합니다.
+            Elements titleElements = document.select(".news-list-title > a");
+
+            // 게시글 제목을 출력합니다.
+            for (Element element : titleElements) {
+                String title = element.text();
+                System.out.println(title);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
