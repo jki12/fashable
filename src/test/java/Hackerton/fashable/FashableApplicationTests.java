e62ea990480a2b9be1aeea42c49dad0c499d7516
Hwangseoeun <sunny030921@gmail.com>
2023-08-06 10:03:58 +0900

    init

---
+++ src/test/java/Hackerton/fashable/FashableApplicationTests.java
@@ -0,0 +1,13 @@
+package Hackerton.fashable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FashableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
